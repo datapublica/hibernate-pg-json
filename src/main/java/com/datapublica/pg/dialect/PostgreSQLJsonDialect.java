70bc2e62668ec48a3096bcb4f249c5487a3f437c
Loïc Petit <loic@MacBook-Pro-de-Loic.local>
2015-10-12 15:44:42 +0200

    Making dialect usable for hibernate 4

--- src/main/java/com/datapublica/pg/dialect/PostgreSQLJsonDialect.java
+++ src/main/java/com/datapublica/pg/dialect/PostgreSQLJsonDialect.java
@@ -1,11 +1,12 @@
 package com.datapublica.pg.dialect;
 
-import org.hibernate.dialect.PostgreSQL92Dialect;
+import org.hibernate.dialect.PostgreSQL9Dialect;
 
 import java.sql.Types;
 
-public class PostgreSQLJsonDialect extends PostgreSQL92Dialect {
+public class PostgreSQLJsonDialect extends PostgreSQL9Dialect {
     public PostgreSQLJsonDialect() {
+        registerColumnType(Types.JAVA_OBJECT, "json");
         registerColumnType(Types.JAVA_OBJECT - 1, "jsonb");
     }
 }
